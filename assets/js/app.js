6e211b3fe6137839fb39362f57a20f3ff0799523
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-06 12:51:28 -0300

    creating functions file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
   initMap();
 });
-
-function initMap() {
-  let mapProp = {
-    center: new google.maps.LatLng(51.508742,-0.120850),
-    zoom: 5
-  };
-  let map = new google.maps.Map(document.getElementById("google-map"), mapProp);
-}
---
+++ assets/js/functions.js
@@ -0,0 +1,3 @@
+const initMap = () => {
+  console.log('holi');
+};
