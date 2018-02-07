a1412f80027354c430eac70b31d987aa3b6b74e9
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-07 11:35:11 -0300

    por descartar xd

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,21 @@
 const initMap = () => {
-  console.log('holi');
+  let myPosition;
+  getLocation();
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: myPosition
+  });
+};
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      myPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+    });
+  } else {
+    alert(`Geolocation is not supported by this browser.`);
+  }
 };
