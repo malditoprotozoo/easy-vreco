5d5a15b08d96fd7ef972344ffe46efb196beab99
SabrinaVillalobos <wafflish@gmail.com>
2018-02-07 01:01:51 -0300

    localization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   initMap();
+  findMee();
 });
 
 function initMap() {
@@ -8,4 +9,25 @@ function initMap() {
     zoom: 5
   };
   let map = new google.maps.Map(document.getElementById("google-map"), mapProp);
+}
+
+function findMe(){
+  var output = document.getElementById('google-map');
+  // verificar si soporta geolocalizacion
+  if(navigator.geolocation){
+    output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
+  }else{
+    output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
+  }
+  // obtener latitud y longitud
+  function localizacion(posicion){
+    var latitude = posicion.coords.latitude;
+    var longitude = posicion.coords.longitude;
+
+    output.innerHTML = "<p>Latitud: "+latitude+" <br> Longitud: "+longitude+"</p>";
+  }
+  function error(){
+    output.innerHTML = "<p>No se pudo obtener tu ubicación</p>"
+  }
+  navigator.geolocation.getCurrentPosition(localizacion,error);
 }
