326e1e177f114a10e0b36d17a08becf231374e11
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-07 12:10:05 -0300

    Removing unnecesary functions and adding comments

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,14 +1,27 @@
+// Definir estas variables fuera de la función para que sean globales
 let map;
 let infoWindow;
 
+/*
+* Función que comenzará cuando se cargue el documento. Normalmente Google
+* la agrega dentro del script como &callback=initMap, pero dado que estamos
+* trabajando con jQuery parecía mejor hacer que se iniciara al cargar el sitio.
+* La función también ha sido "traducida" a ES6, por cuestiones de aprendizaje.
+*/
 const initMap = () => {
-  //let myPosition;
-  //getLocation();
+  // La variable map será el mapa que creará el script de Google en nuestro ID map
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: {lat: -34.397, lng: 150.644}
   });
 
+  /* InfoWindow es una función de Google que permite dibujar sobre el mapa.
+  * Primero, creamos una variable que va a contener la función, para mantenerla
+  * "abreviada" y luego, si el navegador cuenta con geolocalización y el usuario
+  * permite que obtengamos su ubicación, le vamos pasando las opciones a la función
+  * InfoWindow. 
+  * Más info: https://developers.google.com/maps/documentation/javascript/infowindows?hl=es-419
+  */
   infoWindow = new google.maps.InfoWindow;
 
   if (navigator.geolocation) {
@@ -32,18 +45,5 @@ const initMap = () => {
         'Error: Your browser doesn\'t support geolocation.');
         infoWindow.open(map);
     }
-  }
-};
-
-const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      myPosition = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      }
-    });
-  } else {
-    alert(`Geolocation is not supported by this browser.`);
   }
 };
