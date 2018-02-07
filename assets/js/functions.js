5b831dbc08cca5081b17ed06d0e03dd8a2e5bc10
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-07 11:58:12 -0300

    geolocation ready

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,38 @@
+let map;
+let infoWindow;
+
 const initMap = () => {
-  let myPosition;
-  getLocation();
+  //let myPosition;
+  //getLocation();
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
-    center: myPosition
+    center: {lat: -34.397, lng: 150.644}
   });
+
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+  }
 };
 
 const getLocation = () => {
